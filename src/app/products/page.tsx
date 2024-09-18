8af7e4e9f0a2a487fe4cef5ed7513712ed766225
Santhoshkumarz <id.santhoshk@gmail.com>
2024-09-18 15:35:12 +0530

    fix react query

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+
+import ProductComponent from '@/page-component/inventory/products/Product'
+import React from 'react'
+
+const ProductPage = () => {
+  return (
+    <div><ProductComponent/> </div>
+  )
+}
+
+export default ProductPage
---
+++ src/page-component/inventory/products/Product.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductComponent = () => {
+  return <div>products</div>;
+};
+
+export default ProductComponent;
